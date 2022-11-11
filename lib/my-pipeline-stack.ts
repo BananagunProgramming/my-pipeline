26d27a30d63f378953e6f43ca47cd0f6ab058781
David Nash <bananagun@live.com>
2022-11-11 08:10:59 -0500

    Update my-pipeline-stack.ts
    forgot to save this

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('BananagunProgramming/my-pipeline', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "460032395895", region: "us-east-1"}
+    }));
+
+    testingStage.addPost( new ManualApprovalStep('approval'));
   }
 }
