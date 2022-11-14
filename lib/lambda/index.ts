51889cf0ab18bd1e20a9e4b194fbe0553ef32604
David Nash <bananagun@live.com>
2022-11-13 19:49:46 -0500

    change message response from lambda

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -1,7 +1,7 @@
 export async function handler(event: string, context: string) {
     console.log('stage from event: ' + process.env.stage);
     return {
-        body: 'This better work nerd',
+        body: 'This better work you bonus neeeeerrrrrd',
         statusCode: 200
     };
 }
