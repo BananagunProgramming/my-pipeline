1806edb268d3c6f5dbb28406310c4518bf89a8ec
David Nash <bananagun@live.com>
2022-11-13 19:06:10 -0500

    add api gateway

--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -1,17 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as path from 'path';
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props) ;
 
-      new Function(this, 'LambdaFunction', {
+      const handler = new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_16_X,
         handler: 'index.handler',
-        //code: new InlineCode('exports.handler = _ => "Hello, CDK";')
         code: Code.fromAsset(path.join(__dirname, 'lambda')) //resolves to ./lambda
       });
+
+      const api = new apigateway.RestApi(this, 'mygateway', {
+        restApiName: 'myapigateway',
+        description: 'All traf fic goes through this'
+      });
+
+      const items = api.root.addResource('items');
+
+      const apilambdaintegration = new apigateway.LambdaIntegration(handler);
+
+      items.addMethod("GET", apilambdaintegration);
     }
 }
