dccd45ff9734ec8f90fa71a907054cbe0d7f75f1
David Nash <bananagun@live.com>
2022-11-16 19:57:08 -0500

    name stage devqa

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -15,7 +15,7 @@ export class MyPipelineStack extends cdk.Stack {
       })
     });
 
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test phase", {
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "devqa", {
       env: { 
         account: "460032395895", 
         region: "us-east-1"}
